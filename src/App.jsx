05941c711143ffd4252eedb3e61e7e9a62e3f1ec
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-05-04 20:03:00 +0330

    add filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
 import Category from './components/Category'
+import Filter from './components/Filter'
 import Header from './components/Header'
 import ProductList from './components/ProductList'
 import Products from './components/Products'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
 	const [categories, setCategories] = useState([])
 	const [products, setProducts] = useState([])
+	const [filteredProducts, setFilteredProducts] = useState([])
+	const [sort, setSort] = useState('')
+	const [searchValue, setSearchValue] = useState('')
+
+	useEffect(() => {
+		let result = products
+		result = filterSearchTitle(result)
+		result = sortDate(result)
+		setFilteredProducts(result)
+	}, [products, sort, searchValue])
+
+	const searchHandler = e => {
+		setSearchValue(e.target.value.trim().toLowerCase())
+	}
+	const sortHandler = e => {
+		setSort(e.target.value)
+	}
+	const filterSearchTitle = array => {
+		return array.filter(p =>
+			p.title.toLowerCase().includes(searchValue),
+		)
+	}
+	const sortDate = array => {
+		let sortedProducts = [...array]
+		return sortedProducts.sort((a, b) => {
+			if (sort === 'latest') {
+				return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1
+			} else if (sort === 'earliest') {
+				return new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1
+			}
+		})
+	}
 	return (
 		<div className="bg-slate-800 min-h-screen">
 			<Header />
@@ -20,7 +53,16 @@ function App() {
 					products={products}
 					setProducts={setProducts}
 				/>
-				<ProductList products={products} setProducts={setProducts} />
+				<Filter
+					searchValue={searchValue}
+					onSort={sortHandler}
+					onSearch={searchHandler}
+					sort={sort}
+				/>
+				<ProductList
+					products={filteredProducts}
+					setProducts={setProducts}
+				/>
 			</div>
 		</div>
 	)
---
+++ src/components/Filter.jsx
@@ -0,0 +1,52 @@
+function Filter({ onSort, sort, onSearch, searchValue }) {
+	return (
+		<div>
+			<div className="flex items-center justify-between mb-6">
+				<label
+					htmlFor="search-input"
+					className="text-slate-500 text-lg"
+				>
+					search
+				</label>
+				<input
+					type="text"
+					name="search-input"
+					id="search-input"
+					className="bg-transparent rounded-xl border border-slate-500 text-slate-400"
+					value={searchValue}
+					onChange={onSearch}
+				/>
+			</div>
+			<div className="flex items-center justify-between mb-6">
+				<label
+					htmlFor="sort-products"
+					className="text-slate-500 text-lg"
+				>
+					sort
+				</label>
+				<select
+					name="sort-products"
+					id="sort-products"
+					className="bg-transparent text-slate-400 rounded-xl"
+					value={sort}
+					onChange={onSort}
+				>
+					<option
+						className="bg-slate-500 text-slate-300"
+						value="latest"
+					>
+						latest
+					</option>
+					<option
+						className="bg-slate-500 text-slate-300"
+						value="earliest"
+					>
+						earliest
+					</option>
+				</select>
+			</div>
+		</div>
+	)
+}
+
+export default Filter
